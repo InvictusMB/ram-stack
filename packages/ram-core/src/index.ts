70e44d3cfb25bb50d31cc56713df6553db6bc516
InvictusMB <invictusmb@gmail.com>
2020-05-23 14:00:53 +0200

    feat(core): Strictly type container resolution

--- packages/ram-core/src/index.ts
+++ packages/ram-core/src/index.ts
@@ -1,12 +1,22 @@
-export {asClass, asValue, asFunction, AwilixContainer as Container} from 'awilix';
+export {asClass, asValue, asFunction} from 'awilix';
 export * from 'mobx';
 export * from 'mobx-react-lite';
 export {task} from 'mobx-task';
 
-import {createContainer as createAwilixContainer, InjectionMode} from 'awilix';
+import {
+  createContainer as createAwilixContainer,
+  InjectionMode,
+  AwilixContainer,
+  ResolveOptions,
+} from 'awilix';
 
-export function createContainer() {
+export function createContainer<T extends object>(): Container<T> {
   return createAwilixContainer({
     injectionMode: InjectionMode.PROXY,
   });
+}
+
+export type Container<T extends object> = Omit<AwilixContainer<T>, 'resolve'> & {
+  resolve<K extends (string | symbol)>(name: K, resolveOptions?: ResolveOptions): K extends keyof T ? T[K] : never;
+  resolve<V>(name: string | symbol, resolveOptions?: ResolveOptions): V;
 }
--- packages/ram-sample-app/src/index.tsx
+++ packages/ram-sample-app/src/index.tsx
@@ -1,3 +1,4 @@
+import type {InjectedDependencies} from '@ram-stack/context';
 import {registerModule} from '@ram-stack/composition-root/macro';
 import {asClass, asValue, createContainer} from '@ram-stack/core';
 import React from 'react';
@@ -6,7 +7,7 @@ import {createInjector} from './with-container';
 
 import './style.css';
 
-const container = createContainer();
+const container = createContainer<InjectedDependencies>();
 
 const withContainer = createInjector(container);
 
@@ -17,19 +18,18 @@ registerModule({
   withContainer,
 }, '.');
 
-const AppView = container.resolve('AppView') as any;
-const CreateScope = container.resolve('CreateScope') as any;
+const Shell = container.resolve('Shell');
 
 render((
   <div>
-    <CreateScope>
+    <Shell.CreateScope>
       <h2>App 1</h2>
-      <AppView />
-    </CreateScope>
-    <CreateScope>
+      <Shell.AppView />
+    </Shell.CreateScope>
+    <Shell.CreateScope>
       <h2>App 2</h2>
-      <AppView />
-    </CreateScope>
+      <Shell.AppView />
+    </Shell.CreateScope>
   </div>
 ), document.getElementById('root'));
 
